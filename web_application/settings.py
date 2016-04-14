64d8e23e4328c68d9f0e53a63fe7862b9b3f3dfb
LazerCube <git@lazercube.com>
2016-04-14 14:49:47 +0100

    Static files now working

--- web_application/settings.py
+++ web_application/settings.py
@@ -52,16 +52,19 @@ ROOT_URLCONF = 'web_application.urls'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static/')
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' #specifies what url should static files map to under
 
-TEMPLATE_ROOT = (os.path.join(STATIC_ROOT, 'templates/'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static/'),
+) #specifies all the folders on your system where Django should look for static files
 
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static_root/')) # This setting specifies where Django will copy all the static files to
+STATIC_TEMPLATES = (os.path.join(BASE_DIR, 'static/templates/'))
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_ROOT],
+        'DIRS': [STATIC_TEMPLATES],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
