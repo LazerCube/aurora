8561eaa39ca77a7aec8a0107d0551ce2a9d973e3
Elliot <you@example.com>
2016-04-20 11:37:53 +0100

    Started work on log in

--- web_application/urls.py
+++ web_application/urls.py
@@ -5,6 +5,8 @@ from web_application import views
 
 urlpatterns = [
     url(r'^test_ajax/$', views.test_ajax, name='ajax'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^home/$', views.home, name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
 ]
--- web_application/views.py
+++ web_application/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 import json
 
@@ -16,6 +18,27 @@ def test_ajax(request):
         )
     else:
         return HttpResponseForbidden()
+
+def login(request):
+    if request.method =='POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect('/home')
+
+        else:
+            print("Failed")
+            return HttpResponseRedirect('/')
+
+    else:
+        return HttpResponseForbidden()
+
+def home(request):
+    return render(request, 'index/index.html', {})
 
 def index(request):
     return render(request, 'index/index.html', {})
