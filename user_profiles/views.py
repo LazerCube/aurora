99846e839af1d2bb06b5f695a1aab4c835cc42f5
LazerCube <git@lazercube.com>
2016-05-08 22:14:59 +0100

    Requires login

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
 from authentication.permissions import is_owner
 
 from authentication.models import Account
 
+@login_required
 def profile(request, username):
     profile = get_object_or_404(Account, username=username)
     has_permission = is_owner(request, profile)
