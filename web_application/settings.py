08dae3d91c49de72a06bbfbf312c7d60e15412bc
LazerCube <git@lazercube.com>
2016-05-09 16:35:57 +0100

    Changed location of staticfiles

--- web_application/settings.py
+++ web_application/settings.py
@@ -52,13 +52,14 @@ ROOT_URLCONF = 'web_application.urls'
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static-files'),
 ) #specifies all the folders on your system where Django should look for static files
 
 STATIC_URL = '/static/' #specifies what url should static files map to under
 
 STATIC_ROOT = (os.path.join(BASE_DIR, 'static')) # This setting specifies where Django will copy all the static files to
 STATIC_TEMPLATES = (os.path.join(BASE_DIR, 'templates/'))
+
 
 TEMPLATES = [
     {
