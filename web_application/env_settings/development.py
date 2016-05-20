3237174e63b1b11925bfcb8a7f4013e4ac2c46ec
LazerCube <git@lazercube.com>
2016-05-20 16:16:23 +0100

    Removed fixtures. Updated migrations

--- web_application/env_settings/development.py
+++ web_application/env_settings/development.py
@@ -3,10 +3,5 @@ from web_application.settings import *
 
 DEBUG = True
 
-FIXTURE_DIRS = (
-    os.path.join(BASE_DIR, 'web_application', 'fixtures'),
-)
-
-
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
