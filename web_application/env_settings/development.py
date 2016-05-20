88ae40846a3763bf0571fdc9ef4ea5594da98dfe
LazerCube <git@lazercube.com>
2016-05-20 12:26:38 +0100

    Added fixture dirs into dev settings

--- web_application/env_settings/development.py
+++ web_application/env_settings/development.py
@@ -1,6 +1,12 @@
+import os
 from web_application.settings import *
 
 DEBUG = True
+
+FIXTURE_DIRS = (
+    os.path.join(BASE_DIR, 'fixtures'),
+)
+
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
